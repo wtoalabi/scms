e668ca85cd9f9b4960b2276b6bfee05a92ad4b00
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-04 17:58:29 +0100

    Admin menu permissions wip

--- resources/js/admin/pages/Utils/Nav/partials/admin_menu_accounts_management.vue
+++ resources/js/admin/pages/Utils/Nav/partials/admin_menu_accounts_management.vue
@@ -1,50 +1,57 @@
 <template>
     <div>
-        <v-list-group
-            key="settings"
-            v-model="accounts_management.model"
-            :prepend-icon="accounts_management.model ? 'mdi-chevron-up':'mdi-chevron-down'"
-            append-icon=""
-        >
-            <template v-slot:activator >
-                <v-list-item-content>
-                    <v-list-item-title>
-                        <p class="menu-group-title">Accounts</p>
-                    </v-list-item-title>
-                </v-list-item-content>
-            </template>
-            <v-list-item
-                key="users"
-                link
+        <template v-if="groupPermission(permits)">
+            <v-list-group
+                key="settings"
+                v-model="accounts_management.model"
+                :prepend-icon="accounts_management.model ? 'mdi-chevron-up':'mdi-chevron-down'"
+                append-icon=""
             >
-                <v-list-item-action >
-                    <v-icon>mdi-account-group-outline</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>
-                        Users
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item
-                key="admins"
-                link
-            >
-                <v-list-item-action >
-                    <v-icon>mdi-account-cog</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>
-                        Administrators
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list-group>
+                <template v-slot:activator>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            <p class="menu-group-title">Accounts</p>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </template>
+                <v-list-item
+                    v-if="userCan('VIEW_USERS')"
+                    key="users"
+                    link
+                >
+                    <v-list-item-action>
+                        <v-icon>mdi-account-group-outline</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            Users
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item
+                    v-if="userCan('VIEW_ADMINS')"
+                    key="admins"
+                    link
+                >
+                    <v-list-item-action>
+                        <v-icon>mdi-account-cog</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            Administrators
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+        </template>
     </div>
 </template>
 
 <script>
+    import groupMenuPermission from './menu_permission'
+
     export default {
+        mixins: [groupMenuPermission],
         data() {
             return {
                 accounts_management: {
@@ -53,7 +60,14 @@
             }
         },
         methods: {},
-        computed: {}
+        computed: {
+            permits() {
+                return [
+                    "VIEW_USERS",
+                    "VIEW_ADMINS",
+                ];
+            }
+        }
     }
 
 </script>
--- resources/js/admin/store/state/index.js
+++ resources/js/admin/store/state/index.js
@@ -2,7 +2,13 @@ export default{
     darkTheme:true,
     admin: {
         role: {
-            permissions:[]
+            permissions:[
+                "VIEW_USERS",
+                "VIEW_ADMINS",
+                "VIEW_PAYMENTS",
+                "VIEW_SMS_API",
+                "VIEW_EMAIL_API",
+            ]
         }
     }
 }
