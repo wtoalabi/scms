036e6cff50427370109be6bcba452ca6c6468517
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-08 17:49:33 +0100

    wip

--- app/EntitySetup/Stubs/Resources/Resources.php
+++ app/EntitySetup/Stubs/Resources/Resources.php
@@ -19,12 +19,13 @@
             ScaffoldCommand::makeDirectory($path);
             $dummyCollection = $data['file']->get("App/EntitySetup/Stubs/Resources/DummyCollection.stub");
             $dummyResource = $data['file']->get("App/EntitySetup/Stubs/Resources/DummyResource.stub");
-            static::CreateResource($path,$data,$dummyCollection, "{$data['name']}Collection");
-            static::CreateResource($path,$data, $dummyResource,"{$data['name']}Resource");
+            static::CreateResource($data,$dummyCollection, "{$data['name']}Collection");
+            static::CreateResource($data, $dummyResource,"{$data['name']}Resource");
             session('data')['command']->info('Resources created Successfully!');
         }
 
-        private static function CreateResource($path,$data, $content, $name = "") {
+        private static function CreateResource($data, $content, $name = "") {
+            $path = "{$data['namespace']}\\{$data['pluralizedName']}\Resources";
             $resourceContent = str_replace(['Namespace', 'Dummies', 'Dummy', 'dummy'],
                 ["$path;", $data['pluralizedName'], $data['name'], Str::lower($data['name'])], $content);
             $data['file']->put("$path/$name.php", $resourceContent);
