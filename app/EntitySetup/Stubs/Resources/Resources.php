abe5e28f9ab6e84e4762909ef49c8839dc62ef95
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-08 17:45:34 +0100

    wip

--- app/EntitySetup/Stubs/Resources/Resources.php
+++ app/EntitySetup/Stubs/Resources/Resources.php
@@ -1,33 +1,34 @@
 <?php
-  /**
-   * Created by Alabi Olawale
-   * Date: 6/4/2019
-   */
-  declare(strict_types=1);
-  
-  namespace App\EntitySetup\Stubs\Resources;
-  
-  
-  use App\EntitySetup\ScaffoldCommand;
-  use Illuminate\Support\Str;
+    /**
+     * Created by Alabi Olawale
+     * Date: 6/4/2019
+     */
+    declare(strict_types=1);
 
-  class Resources
-  {
-    public static function SetUp() {
-      $data = session('data');
-      $path = "{$data['namespace']}\\{$data['pluralizedName']}\Resources";
-      ScaffoldCommand::makeDirectory($path);
-      $dummyCollection = $data['file']->get("App\EntitySetup\Stubs\Resources\DummyCollection.stub");
-      $dummyResource = $data['file']->get("App\EntitySetup\Stubs\Resources\DummyResource.stub");
-      static::CreateResource($path,$data,$dummyCollection, "{$data['name']}Collection");
-      static::CreateResource($path,$data, $dummyResource,"{$data['name']}Resource");
-      session('data')['command']->info('Resources created Successfully!');
+    namespace App\EntitySetup\Stubs\Resources;
+
+
+    use App\EntitySetup\ScaffoldCommand;
+    use Illuminate\Support\Str;
+
+    class Resources
+    {
+        public static function SetUp() {
+            $data = session('data');
+            $path = "{$data['pathName']}/{$data['pluralizedName']}/Resources";
+            dd($path);
+            ScaffoldCommand::makeDirectory($path);
+            $dummyCollection = $data['file']->get("App/EntitySetup/Stubs/Resources/DummyCollection.stub");
+            $dummyResource = $data['file']->get("App/EntitySetup/Stubs/Resources/DummyResource.stub");
+            static::CreateResource($path,$data,$dummyCollection, "{$data['name']}Collection");
+            static::CreateResource($path,$data, $dummyResource,"{$data['name']}Resource");
+            session('data')['command']->info('Resources created Successfully!');
+        }
+
+        private static function CreateResource($path,$data, $content, $name = "") {
+            $resourceContent = str_replace(['Namespace', 'Dummies', 'Dummy', 'dummy'],
+                ["$path;", $data['pluralizedName'], $data['name'], Str::lower($data['name'])], $content);
+            $data['file']->put("$path/$name.php", $resourceContent);
+        }
+
     }
-  
-    private static function CreateResource($path,$data, $content, $name = "") {
-      $resourceContent = str_replace(['Namespace', 'Dummies', 'Dummy', 'dummy'],
-        ["$path;", $data['pluralizedName'], $data['name'], Str::lower($data['name'])], $content);
-      $data['file']->put("$path\\$name.php", $resourceContent);
-    }
-    
-  }
