c7ec0a357fb942c197791b9e84767d52a582e119
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-08 08:21:01 +0100

    wip

--- app/EntitySetup/Stubs/Requests/Requests.php
+++ app/EntitySetup/Stubs/Requests/Requests.php
@@ -4,46 +4,46 @@
    * Date: 6/1/2019
    */
   declare(strict_types=1);
-  
+
   namespace App\EntitySetup\Stubs\Requests;
-  
-  
+
+
   use App\EntitySetup\ScaffoldCommand;
   use Illuminate\Support\Str;
 
   class Requests{
-  
+
     public static function SetUp() {
       $data = session('data');
       static::moveBaseRequest($data);
       static::generateRequestStubs($data);
       $data['command']->info('Form Request files for ' . $data['name']. ' have been created.');
     }
-    
-  
+
+
     private static function moveBaseRequest($data) {
-      if(!$data['file']->exists("{$data['base']}\Base\BaseRequest")) {
-        $data['file']->copy("App\EntitySetup\Stubs\Requests\DummyBaseRequest.stub",
-          "{$data['base']}\Base\BaseFormRequest.php");
+      if(!$data['file']->exists("{$data['base']}/Base/BaseRequest")) {
+        $data['file']->copy("App/EntitySetup/Stubs/Requests/DummyBaseRequest.stub",
+          "{$data['base']}/Base/BaseFormRequest.php");
       }
     }
-  
+
     private static function generateRequestStubs($data) {
-      $modelName = "{$data['namespace']}\\{$data['pluralizedName']}\Requests";
+      $modelName = "{$data['namespace']}//{$data['pluralizedName']}/Requests";
       static::stub("$modelName", "Create", $data);
       static::stub("$modelName", "Update", $data);
     }
-  
+
     private static function stub(string $destination, $action,$data) {
-      
-      $destinationFile = "$destination\\$action{$data['name']}FormRequest.php";
+
+      $destinationFile = "$destination$action{$data['name']}FormRequest.php";
       ScaffoldCommand::makeDirectory("$destination");
-      $source = app_path('EntitySetUp\Stubs\Requests\DummyRequest.stub');
+      $source = app_path('EntitySetUp/Stubs/Requests/DummyRequest.stub');
       $dummyContent = $data['file']->get($source);
       $requestContent = str_replace(['Dummy', 'Dummies', 'Action','Path'],
         [$data['name'], $data['pluralizedName'], $action, $data['namespace']], $dummyContent);
       $data['file']->put("$destinationFile", $requestContent);
     }
-  
-  
+
+
   }
