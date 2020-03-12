f761d5eebdbc835245577345b9d1551861060b6e
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-12 18:34:10 +0100

    wip

--- app/EntitySetup/Stubs/Permissions/Permissions.php
+++ app/EntitySetup/Stubs/Permissions/Permissions.php
@@ -45,7 +45,7 @@
         private static function CreatePermissionSeedForEntity() {
             $data = session('data');
             $dummyContent = $data['file']->get('App/EntitySetUp/Stubs/Permissions/EntityPermission.stub');
-            $namespace = "{$data['namespace']}\Base\Authorization\Permission";
+            $namespace = "{$data['base']}\Base\Authorization\Permission";
             $destinationPath = database_path("seeds/Authorization/Permissions//{$data['name']}PermissionsSeeder.php");
             $repositoryContent = str_replace(
                 ['Dummy', 'Dummies', 'DUMMY', 'PermissionModel',],
