899ec455f46be2ab80d74632948646500ceb1f6b
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-17 14:05:01 +0100

    Added loading actions file

--- resources/js/user/store/actions/index.js
+++ resources/js/user/store/actions/index.js
@@ -1,7 +1,9 @@
 import contact_actions from "./contact_actions";
 import user_actions from "./user_actions";
+import loading_actions from "./loading_actions";
 
 export default {
     ...contact_actions,
-    ...user_actions
+    ...user_actions,
+    ...loading_actions
 }
