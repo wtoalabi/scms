43498c85e49141fd1709d42744a48752585c1957
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2020-03-17 14:00:29 +0100

    Refactored how baseuRL is consumed

--- resources/js/user/store/actions/contact_actions.js
+++ resources/js/user/store/actions/contact_actions.js
@@ -1,7 +1,7 @@
 import Request from "../../../utils/form/StateFulRequest";
 export default {
     async loadContacts(store) {
-        await Request(`${_globals.userBaseUrl}/contacts`, {
+        await Request(`${_globals.baseUrl()}/contacts`, {
             action : "post",
             mutator: 'commitContacts',
             store,
--- resources/js/utils/GlobalStates.js
+++ resources/js/utils/GlobalStates.js
@@ -1,6 +1,6 @@
 export default {
-    userBaseUrl : "https://account.scms.loc",
+    /*userBaseUrl : "https://account.scms.loc",
     adminBaseUrl : "https://admin.scms.loc",
-    baseUrl : "https://scms.loc",
-
+    baseUrl : "https://scms.loc",*/
+    baseUrl : ()=> location.origin,
 }
